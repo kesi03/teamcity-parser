7ad2837248cd3bdac1e151ee23b0c1186ec5559f
admin <admin@TeamCity>
2025-11-03 12:58:26 +0100

    'chains' build template was created (TeamCity change in 'mockholm' project)

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -25,4 +25,10 @@ To debug in IntelliJ Idea, open the 'Maven Projects' tool window (View
 version = "2025.07"
 
 project {
+
+    template(Chains)
 }
+
+object Chains : Template({
+    name = "chains"
+})
